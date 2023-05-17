ef5d2822627d7761b475bbaf35ea0a61dcd30ab0
Martín Berlanda <41312800+martinberlanda@users.noreply.github.com>
2023-05-17 15:11:09 -0300

    Update inspirationBot.js

--- inspirationBot.js
+++ inspirationBot.js
@@ -4,23 +4,25 @@ let chatMessage = "";
 // Get all active players
 let players = game.users.filter(user => user.active && !user.isGM && user.character);
 
-// Loop through each player and get their character sheet
-players.forEach(player => {
-  let character = player.character;
+if (players.length == 0) {
+  chatMessage = "No players connected"
+} else {
+  chatMessage = "Characters with Inspiration:<br>"
+  // Loop through each player and get their character sheet
+  players.forEach(player => {
+    let character = player.character;
 
-  // Check if the character sheet exists and has an "inspiration" field
-  if (character && character.system.attributes.inspiration) {
-
-    // Get the "inspiration" field value and add it to the chat message
-    let inspirationValue = character.system.attributes.inspiration;
-    let message = inspirationValue ? "Yes" : "No";
-    chatMessage += `${character.name}: ${message}<br>`;
-  }
-});
+    // Check if the character sheet exists and has inspiration
+    if (character && character.system.attributes.inspiration) {
+      // Adds the character to the message
+      chatMessage += `${character.name}<br>`;
+    }
+  });
+}
 
 // Set up the chat message data object
 chatData.content = chatMessage;
-chatData.speaker = ChatMessage.getSpeaker({alias: "Inspiration bot"});
+chatData.speaker = ChatMessage.getSpeaker({ alias: "Inspiration bot" });
 
 // Send the chat message to the chat log
 ChatMessage.create(chatData);
